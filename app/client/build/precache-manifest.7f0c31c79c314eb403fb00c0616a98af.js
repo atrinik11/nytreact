07896f1efa81c186dd9c410820bb9aca93348158
Reena Mahapatra <reenam2017@gmail.com>
2018-10-13 23:15:53 -0700

    heroku add

---
+++ app/client/build/precache-manifest.7f0c31c79c314eb403fb00c0616a98af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "48ff19380c8e93d73d2d",
+    "url": "/static/js/main.48ff1938.chunk.js"
+  },
+  {
+    "revision": "4de9f85c9ab5b64a434a",
+    "url": "/static/js/1.4de9f85c.chunk.js"
+  },
+  {
+    "revision": "48ff19380c8e93d73d2d",
+    "url": "/static/css/main.82c2ec53.chunk.css"
+  },
+  {
+    "revision": "256480b4094f79acaa4f2d5321d7438b",
+    "url": "/index.html"
+  }
+];
